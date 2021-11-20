0a9d875b1733bfcff3f1f5e23cc03edb8c19cd82
may <may1092200258@gmail.com>
2021-11-20 12:19:36 +0800

    ready to submit

--- src/pages/RouteMap.js
+++ src/pages/RouteMap.js
@@ -7,7 +7,7 @@ import SearchBar from "../components/Roadmap/SearchBar";
 function Roadmap() {
   return (
     <div className="Roadmap content-full relative mt-header">
-      <div className="absolute left-1/2 z-10 w-max mt-10 grid gap-5 grid-flow-col -translate-x-50">
+      {/* <div className="absolute left-1/2 z-10 w-max mt-10 grid gap-5 grid-flow-col -translate-x-50">
         <NavLink
           to="/route/district"
           className="btn block text-white bg-gray-default hover:bg-black"
@@ -24,7 +24,7 @@ function Roadmap() {
         >
           常用路線
         </NavLink>
-      </div>
+      </div> */}
       <div className="w-max m-0 py-10 px-10 flex absolute z-10 shadow-default">
         <Route path="/route/district">
           <SearchBar />
