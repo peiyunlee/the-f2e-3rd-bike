aa548f94874e6a7b92d5e475abd5b906052262d7
may <may1092200258@gmail.com>
2021-12-25 01:17:47 +0800

    style - header login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from "../assets/logo.png";
-import { NavLink,Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { useState } from "react";
-import { ReactComponent as MenuIcon } from '../assets/icon/menu.svg'
-import { ReactComponent as CloseIcon } from '../assets/icon/close.svg'
+import { ReactComponent as MenuIcon } from "../assets/icon/menu.svg";
+import { ReactComponent as CloseIcon } from "../assets/icon/close.svg";
+import { ReactComponent as UserIcon } from "../assets/icon/user.svg";
+import { ReactComponent as ArrowIcon } from "../assets/icon/arrow-right.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-function Header({setshowAuth}) {
-  const [show, setshow] = useState(false)
+function Header({ setshowAuth }) {
+  const [show, setshow] = useState(false);
 
-  const _HndleClickBtn = (type = 'toggle') => {
-    if(type === 'hide')
-      setshow(false)
-    else
-      setshow(!show)
-  }
+  const _HndleClickBtn = (type = "toggle") => {
+    if (type === "hide") setshow(false);
+    else setshow(!show);
+  };
 
   return (
     <>
@@ -24,7 +24,7 @@ function Header({setshowAuth}) {
           className="w-max grid grid-col-2 grid-rows-2 grid-flow-col shadow-text"
           to="/"
           activeClassName=""
-          onClick={()=>_HndleClickBtn('hide')}
+          onClick={() => _HndleClickBtn("hide")}
         >
           <img className="row-span-2 mr-3" src={logo} alt="" />
           <h1 className="font-en tracking-widest">TAIWAN BIKE</h1>
@@ -118,18 +118,30 @@ function Header({setshowAuth}) {
             </NavLink>
           </div>
           <div className="group ml-8 w-max h-header flex items-center relative">
-            <a className="nav-link text-yellow-default group-hover:text-yellow-hover" onClick={()=>setshowAuth(true)}>
+            <a
+              className="nav-link text-yellow-default group-hover:text-yellow-hover"
+              onClick={() => setshowAuth(true)}
+            >
               登入
             </a>
+            {/* <UserIcon /> */}
           </div>
         </div>
         <div className="lg:hidden relative" onClick={_HndleClickBtn}>
-          <div className={!show ? 'block' : 'hidden'}><MenuIcon /></div>
-          <div className={show ? 'block' : 'hidden'}><CloseIcon /></div>
+          <div className={!show ? "block" : "hidden"}>
+            <MenuIcon />
+          </div>
+          <div className={show ? "block" : "hidden"}>
+            <CloseIcon />
+          </div>
         </div>
       </div>
-      <div className={`${show ? 'fixed' : "hidden"} h-screen bg-white z-40 w-full px-7 md:px-16 pt-14`}>
-        <div className="flex items-center relative mb-10 md:w-3/5 max-w-md md:max-w-full">
+      <div
+        className={`${
+          show ? "fixed" : "hidden"
+        } h-screen bg-white z-40 w-full px-7 md:px-16 pt-14`}
+      >
+        <div className="flex items-center relative mb-6 md:w-3/5 max-w-md md:max-w-full">
           <input
             className="input-text mb-2 py-2.5"
             type="text"
@@ -139,26 +151,85 @@ function Header({setshowAuth}) {
             <FontAwesomeIcon icon={faSearch} size="lg" color="#333333" />
           </a>
         </div>
+        {/* <a
+          className="mb-4 nav-link group flex items-center flex-row"
+          onClick={() => setshowAuth(true)}
+        >
+          <span className="text-xl text-yellow-default group-hover:text-yellow-hover">
+            登入
+          </span>
+          <ArrowIcon className="ml-3" />
+        </a> */}
+        <div className="mb-4 nav-link group flex items-center flex-row">
+          <UserIcon className="ml-1 mr-4" />
+          <span className="text-xl text-yellow-default">
+            已登入
+          </span>
+        </div>
         <div className="border-b border-gray-light mb-10"></div>
         <div className="grid md:grid-flow-col md:grid-cols-none grid-cols-2 gap-y-10 gap-x-10 md:max-w-full max-w-lg">
           <div className="grid gap-3 justify-start grid-rows-3">
-            <span className="font-bold text-left tracking-wide md:text-xl">最新消息</span>
-            <Link onClick={_HndleClickBtn} to='/news/announcement' className="text-left tracking-wide hover:text-green-hover">最新公告</Link>
-            <Link onClick={_HndleClickBtn} to='/news/activity' className="text-left tracking-wide hover:text-green-hover">活動資訊</Link>
+            <span className="font-bold text-left tracking-wide md:text-xl">
+              最新消息
+            </span>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/news/announcement"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              最新公告
+            </Link>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/news/activity"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              活動資訊
+            </Link>
           </div>
           <div className="grid gap-3 justify-start grid-rows-3">
-            <span className="font-bold text-left tracking-wide md:text-xl">自行車路線</span>
-            <Link onClick={_HndleClickBtn} to='/route/district' className="text-left tracking-wide hover:text-green-hover">區域路線</Link>
+            <span className="font-bold text-left tracking-wide md:text-xl">
+              自行車路線
+            </span>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/route/district"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              區域路線
+            </Link>
             {/* <Link onClick={_HndleClickBtn} to='/route/store' className="text-left tracking-wide hover:text-green-hover">常用路線</Link> */}
           </div>
           <div className="grid gap-3 justify-start grid-rows-3">
-            <span className="font-bold text-left tracking-wide md:text-xl">單車租借</span>
-            <Link onClick={_HndleClickBtn} to='/station/rent' className="text-left tracking-wide hover:text-green-hover">站點資訊</Link>
-            <Link onClick={_HndleClickBtn} to='/station/store' className="text-left tracking-wide hover:text-green-hover">常用路線</Link>
+            <span className="font-bold text-left tracking-wide md:text-xl">
+              單車租借
+            </span>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/station/rent"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              站點資訊
+            </Link>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/station/store"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              常用路線
+            </Link>
           </div>
           <div className="grid gap-3 justify-start grid-rows-3">
-            <span className="font-bold text-left tracking-wide md:text-xl">騎乘小叮嚀</span>
-            <Link onClick={_HndleClickBtn} to='/safety' className="text-left tracking-wide hover:text-green-hover">騎乘小叮嚀</Link>
+            <span className="font-bold text-left tracking-wide md:text-xl">
+              騎乘小叮嚀
+            </span>
+            <Link
+              onClick={_HndleClickBtn}
+              to="/safety"
+              className="text-left tracking-wide hover:text-green-hover"
+            >
+              騎乘小叮嚀
+            </Link>
           </div>
         </div>
       </div>
