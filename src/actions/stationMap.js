32063afcfb93beb39ada243360750fcb08863540
may <may1092200258@gmail.com>
2022-01-02 20:35:39 +0800

    fix

--- src/actions/stationMap.js
+++ src/actions/stationMap.js
@@ -1,19 +1,11 @@
 import {
   SET_STATIONS,
   SET_MAP_CENTER_POS,
-  REFRESH_STATIONS
 } from "../utils/actions/stationMap";
 
 export const setStations = (infoList, availabilityList) => {
   return {
     type: SET_STATIONS,
-    payload: { infoList: infoList, availabilityList: availabilityList },
-  };
-};
-
-export const refreshStations = (infoList, availabilityList) => {
-  return {
-    type: REFRESH_STATIONS,
     payload: { infoList: infoList, availabilityList: availabilityList },
   };
 };
